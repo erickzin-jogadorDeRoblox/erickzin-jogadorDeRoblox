1e5c961b108d363c0c5eff4f55df7831f19d3bba
erickzin-jogadorDeRoblox <programadorqualquerai@gmail.com>
2024-12-19 00:40:38 -0300

    Update and rename README.md to README.lua

--- README.lua
+++ README.lua
@@ -4,9 +4,12 @@
 - 💞️ I’m looking to collaborate on Roblox games or arduino projects
 - 📫 How to reach me my number of zapzap is +55 31 8931-5980 and my discord is programador_qualquer
 - 😄 Pronouns: player of roblox
-- ⚡ Fun fact: roblox is good
+- ⚡ Fun fact: roblox is good and i speek portuguese
 
-<!---
+---
 erickzin-jogadorDeRoblox/erickzin-jogadorDeRoblox is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
 You can click the Preview link to take a look at your changes.
---->
+---
+
+
+print("hello, github")
